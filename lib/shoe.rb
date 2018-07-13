3be828e7478b95e17373d7d8c1e5caa6cdf9788b
monineath <monineathh@gmail.com>
2018-07-13 14:16:31 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,14 @@
 # Make your shoe class here!
 class Shoe
- attr_reader = :brand
- attr_accessor = :color, :size, :material, :condition
-def initialize(brand)
-  @brand = brand
-end
+ attr_reader :brand
+ attr_accessor :color, :size, :material, :condition
+  
+  def initialize(dbfjhdsf)
+    @brand = dbfjhdsf
+  end
+  
 end
   
   adidas = Shoe.new("Adidas")
+  
 puts adidas.brand
